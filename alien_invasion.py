9dfbd761c2d8a33715bb2374a26fafb318f5fee2
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-07-08 21:24:27 +0300

    Add clock attribute to the init method

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,6 +8,7 @@ class AlienInvasion():
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
+        self.clock = pygame.time.Clock()
 
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
